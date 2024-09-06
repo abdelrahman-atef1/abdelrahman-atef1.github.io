d4c2db54e7a466f4915d06bfdf1541c89f3c877e
Abdelrahman Atef <abduu0000@yahoo.com>
2024-09-06 05:25:37 +0300

    delete old project

--- assets/config-BmtdpFjk.js
+++
@@ -1,4 +0,0 @@
-const n="Abdelrahman Atef",o="Developer",t=["Android","IOS","Desktop"],e="https://hamishw.com",i="abdelrahmanatef4",s="abdelrahman-atef-793917167/",a="abdelrahman-atef1",c="https://github.com/HamishMW/portfolio",r=`    ∕\\
-  ∕  \\
- ∕ ∕\\ \\
-∕ ____ \\`,l={name:n,role:o,disciplines:t,url:e,upwork:i,linkedin:s,github:a,repo:c,ascii:r};export{l as c};
